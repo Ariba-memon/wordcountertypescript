23d0a7df36f214bdc2d5b5f58b02b2c1e45318de
Ariba-memon <aribaabdulqadir786@gmail.com>
2023-01-19 01:37:40 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([{
+    name: "wordsknow",
+    type: "input",
+    message: "Hi Enter your words to count and enjoy word counter:"
+},
+{
+    type: "name",
+    name: "Name",
+    message: "Enter Your Name:"
+},
+]);
+console.log(`Hello there ${answer.Name}Your words counter answer is  ${answer.wordsknow.length}`);
